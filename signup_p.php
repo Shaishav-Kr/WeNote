f2fb8561d6819ed944ade08186c9dcf3d80e556c
Shaishav <84002444+Shaishav-Kr@users.noreply.github.com>
2023-02-01 20:14:51 +0530

    signup updated

--- signup_p.php
+++ signup_p.php
@@ -1,14 +1,13 @@
 <?php
 require('db.php');
-include("auth.php");
 
 $sql = "INSERT INTO `user`(`username`, `fname`, `lname`, `email`, `dob`, `phone`, `password`) VALUES ('$_POST[username]','$_POST[first_name]','$_POST[last_name]','$_POST[email]','$_POST[dob]','$_POST[phone]','$_POST[password]')";
 
 if ($conn->query($sql) === TRUE) {
 	echo "<script>
-		alert('Data Registered successfully');
-		window.location.href='login.php';
-		</>";
+	alert('Data Registered successfully');
+	window.location.href='login.php';
+	</script>";
 } else {
 	echo "error";
 	// die();
